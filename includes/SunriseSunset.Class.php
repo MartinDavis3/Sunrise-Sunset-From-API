74e1e1b5d99b57ba4b0d1a21cb9d4abcba23ee0c
Martin Davis <martin_davis3@yahoo.ca>
2020-05-02 18:18:02 -0600

    change to use DateTime class

--- includes/SunriseSunset.Class.php
+++ includes/SunriseSunset.Class.php
@@ -1,55 +1,55 @@
 <?php
 class SunriseSunset {
-  public $sunrise = "";
-  public $sunset = "";
-  public $solar_noon = "";
+  public $sunrise = "0001-01-01T00:00:00+00:00";
+  public $sunset = "0001-01-01T00:00:00+00:00";
+  public $solar_noon = "0001-01-01T00:00:00+00:00";
   public $day_length = 0;
-  public $civil_twilight_begin = "";
-  public $civil_twilight_end = "";
-  public $nautical_twilight_begin = "";
-  public $nautical_twilight_end = "";
-  public $astronomical_twilight_begin = "";
-  public $astronomical_twilight_end = "";
+  public $civil_twilight_begin = "0001-01-01T00:00:00+00:00";
+  public $civil_twilight_end = "0001-01-01T00:00:00+00:00";
+  public $nautical_twilight_begin = "0001-01-01T00:00:00+00:00";
+  public $nautical_twilight_end = "0001-01-01T00:00:00+00:00";
+  public $astronomical_twilight_begin = "0001-01-01T00:00:00+00:00";
+  public $astronomical_twilight_end = "0001-01-01T00:00:00+00:00";
 
   function __construct(
-    $sunrise = "",
-    $sunset = "",
-    $solar_noon = "",
+    $sunrise = "0001-01-01T00:00:00+00:00",
+    $sunset = "0001-01-01T00:00:00+00:00",
+    $solar_noon = "0001-01-01T00:00:00+00:00",
     $day_length = 0,
-    $civil_twilight_begin = "",
-    $civil_twilight_end = "",
-    $nautical_twilight_begin = "",
-    $nautical_twilight_end = "",
-    $astronomical_twilight_begin = "",
-    $astronomical_twilight_end = ""
+    $civil_twilight_begin = "0001-01-01T00:00:00+00:00",
+    $civil_twilight_end = "0001-01-01T00:00:00+00:00",
+    $nautical_twilight_begin = "0001-01-01T00:00:00+00:00",
+    $nautical_twilight_end = "0001-01-01T00:00:00+00:00",
+    $astronomical_twilight_begin = "0001-01-01T00:00:00+00:00",
+    $astronomical_twilight_end = "0001-01-01T00:00:00+00:00"
   )
   {
-    $this->sunrise = $sunrise;
-    $this->sunset = $sunset;
-    $this->solar_noon = $solar_noon;
+    $this->sunrise = new DateTime($sunrise);
+    $this->sunset = new DateTime($sunset);
+    $this->solar_noon = new DateTime($solar_noon);
     $this->day_length = $day_length;
-    $this->civil_twilight_begin = $civil_twilight_begin;
-    $this->civil_twilight_end = $civil_twilight_end;
-    $this->nautical_twilight_begin = $nautical_twilight_begin;
-    $this->nautical_twilight_end = $nautical_twilight_end;
-    $this->astronomical_twilight_begin = $astronomical_twilight_begin;
-    $this->astronomical_twilight_end = $astronomical_twilight_end;
+    $this->civil_twilight_begin = new DateTime($civil_twilight_begin);
+    $this->civil_twilight_end = new DateTime($civil_twilight_end);
+    $this->nautical_twilight_begin = new DateTime($nautical_twilight_begin);
+    $this->nautical_twilight_end = new DateTime($nautical_twilight_end);
+    $this->astronomical_twilight_begin = new DateTime($astronomical_twilight_begin);
+    $this->astronomical_twilight_end = new DateTime($astronomical_twilight_end);
   }
 
-  public function output ()
+  public function output()
   {
     $output = '';
     ob_start();
     ?>
-    <p><?php $this->sunrise; ?></p>
-    <p><?php $this->sunset; ?></p>
-    <p><?php $this->solar_noon; ?></p>
-    <p><?php $this->civil_twilight_begin; ?></p>
-    <p><?php $this->civil_twilight_end; ?></p>
-    <p><?php $this->nautical_twilight_begin; ?></p>
-    <p><?php $this->nautical_twilight_end; ?></p>
-    <p><?php $this->astronomical_twilight_begin; ?></p>
-    <p><?php $this->astronomical_twilight_end; ?></p>
+    <p><?php echo date_format($this->sunrise, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->sunset, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->solar_noon, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->civil_twilight_begin, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->civil_twilight_end, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->nautical_twilight_begin, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->nautical_twilight_end, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->astronomical_twilight_begin, 'H:i:s'); ?></p>
+    <p><?php echo date_format($this->astronomical_twilight_end, 'H:i:s'); ?></p>
     <?php
     $output = ob_get_clean();
     return $output;    
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+  if ( isset( $_GET['reset'] ) )
+  {
+    session_unset();
+    session_destroy();
+    session_start();
+  }
 
-include './includes/SunriseSunset.Class.php'
+include './includes/SunriseSunset.Class.php';
 
 // this is code that sets error reporting to cover all errors
 ini_set( 'display_errors', 1);
@@ -8,20 +14,58 @@ ini_set( 'display_startup_errors', 1);
 error_reporting( E_ALL );
 
   $siteURL = 'https://api.sunrise-sunset.org/json';
-  $latitude = '';
-  $longitude = '';
+  $continent = '';
+  $city = '';
+  $latitude = 0;
+  $longitude = 0;
+  $getTimeZoneFailed = FALSE;
+  $timeOffset = 0;
 
   if ( isset( $_GET ) && !empty( $_GET ) )
   {
-    $latitude = $_GET['latitude'];
-    $longitude = $_GET['longitude'];
+    $continent = $_GET['continent'];
+    $city = $_GET['city'];
+    // $searchURL = $siteURL.'?lat='.$latitude.'&lng='.$longitude.'&formatted=0';
+    try {
+      $dateTime = new DateTime(null, new DateTimeZone($continent.'/'.$city));
+      $dateTimeUTC = new DateTime(null, new DateTimeZone('UTC'));
+      $timeZone = $dateTime->getTimezone();
+      $timeOffset = $timeZone->getOffset($dateTimeUTC);
+    } catch (Exception $e) {
+      //Should really test exception type here - but outside the scope of current project
+      echo 'Continent / City pair not found. Please try again.';
+      $getTimeZoneFailed = TRUE;
+    }
+
+    if ( !$getTimeZoneFailed )
+    {
+      //Get location returns an array with
+      //latitude  and longitude in.
+      $location = $timeZone->getLocation();
+      $latitude = $location['latitude'];
+      $longitude = $location['longitude'];
+      $searchURL = $siteURL.'?lat='.$latitude.'&lng='.$longitude.'&formatted=0';
+
+      $retrievedJSONString = file_get_contents( $searchURL );
+      $retrievedObject = json_decode( $retrievedJSONString );
+      $retrievedSunriseSunset = $retrievedObject->results;
+      $newSunriseSunset = new SunriseSunset(
+        $retrievedSunriseSunset->sunrise,
+        $retrievedSunriseSunset->sunset,
+        $retrievedSunriseSunset->solar_noon,
+        $retrievedSunriseSunset->day_length,
+        $retrievedSunriseSunset->civil_twilight_begin,
+        $retrievedSunriseSunset->civil_twilight_end,
+        $retrievedSunriseSunset->nautical_twilight_begin,
+        $retrievedSunriseSunset->nautical_twilight_end,
+        $retrievedSunriseSunset->astronomical_twilight_begin,
+        $retrievedSunriseSunset->astronomical_twilight_end
+      );
+
+      // $sunriseDateTime = new DateTime($newSunriseSunset->sunrise);
+    }
+
   }
-
-  $searchURL = $siteURL.'?lat='.$latitude.'&lng='.$longitude.'&formatted=0';
-
-  $retrivedSunriseSunsetJSONString = file_get_contents( $searchURL );
-  $retrivedSunriseSunsetObject = json_decode( $retrivedSunriseSunsetJSONString );
-  $retrivedSunriseSunset = $retrivedSunriseSunsetObject->results;
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -33,19 +77,33 @@ error_reporting( E_ALL );
 <body>
   <h1>Sunrise and Sunset</h1>
   <form action="./index.php" method="GET">
-        <label for="username">
-          Latitude:
-          <input type="text" name="latitude" id="latitude">
+    <input type="submit" name="reset" value="Reset">
+        <label for="continent">
+          Continent:
+          <input type="text" name="continent" id="continent">
         </label>
         <label>
-        <label for="password">
-          Longitude:
-          <input type="text" name="longitude" id="longitude">
+        <label for="city">
+          City:
+          <input type="text" name="city" id="city">
         </label>
         </label>
-          <input type="submit" value="Show Times">
+          <input type="submit" name ="show-times" value="Show Times">
         </label>
       </form>
-      <p><?php echo $searchURL; ?>
+      <p><?php
+        if( isset( $timeZone ) )
+        {
+          echo $searchURL;
+          echo $timeZone->getName();
+          echo $timeOffset;
+          echo $newSunriseSunset->output();
+          // echo date_format($newSunriseSunset->sunrise, 'H:i:s');
+        }
+      ?>
+      <pre>
+        <?php var_dump( $timeZone ); ?>
+      </pre>
+
 </body>
 </html>
